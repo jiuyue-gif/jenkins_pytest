14b36965691b7f2f371ae78ddce240edc102147e
liruonan6 <12723273+liruonan6@user.noreply.gitee.com>
2023-06-02 14:28:39 +0800

    second commit

--- scripts_all/test_say_hi.py
+++ scripts_all/test_say_hi.py
@@ -1,19 +1,22 @@
 import allure,pytest
 
 
+
 class TestSayHi(object):
     @pytest.mark.xfail(False, reason="")
     @allure.severity(allure.severity_level.CRITICAL)
-    @allure.step(title="成功打印文字")
+    @allure.feature("成功打印文字模板")
     def test_success(self):
         print("hi, success!!!")
         assert 1
 
     @pytest.mark.xfail(True, reason="")
-    @allure.step(title="断言失败--打印文字")
+    @allure.feature("chagpt给的模板")
     def test_fail(self):
-        print("hi, fail!!!")
-        assert 0
+        with allure.step("Step 1"):
+            assert 1 + 1 == 2
+        with allure.step("Step 2"):
+            assert 2 * 2 == 4
 
 
 # if __name__ == "__main__":
